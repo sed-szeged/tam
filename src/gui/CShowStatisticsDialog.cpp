f68f56d60fa00f869820eafa607a564d5bef8643
David Havas <havasd@inf.u-szeged.hu>
2014-06-18 13:04:50 +0200

    Basic statistics visualization.

--- src/gui/CShowStatisticsDialog.cpp
+++ src/gui/CShowStatisticsDialog.cpp
@@ -30,8 +30,12 @@ void CShowStatisticsDialog::createHtml() {
             "<script type=\"text/javascript\" src=\"https://www.google.com/jsapi\"></script>"
             "<script type=\"text/javascript\">"
               "google.load(\"visualization\", \"1\", {packages:[\"corechart\"]});"
-              //"google.setOnLoadCallback(drawCodeCovChart);"
+              "google.setOnLoadCallback(drawCodeCovChart);"
               "google.setOnLoadCallback(drawTestCovChart);"
+              "google.setOnLoadCallback(drawFailResChart);"
+              "google.setOnLoadCallback(drawRevFailResChart);"
+              "google.setOnLoadCallback(drawExecResChart);"
+              "google.setOnLoadCallback(drawTestCaseInfoResChart);"
 
               "function drawCodeCovChart() {"
                 "var data = google.visualization.arrayToDataTable(["
@@ -46,11 +50,11 @@ void CShowStatisticsDialog::createHtml() {
                 "};"
                 "var chart = new google.visualization.ColumnChart(document.getElementById('code_cov_hist'));"
                 "chart.draw(data, options);"
-               "}"
-               "function drawTestCovChart() {"
+              "}"
+              "function drawTestCovChart() {"
                   "var data = google.visualization.arrayToDataTable(["
                     "['Number of covered code elements in a test case', 'Occurences'],"
-                "%7]);"
+                "%2]);"
 
                 "var options = {"
                     "title: 'Test case coverage histogram',"
@@ -61,40 +65,125 @@ void CShowStatisticsDialog::createHtml() {
                 "var chart = new google.visualization.ColumnChart(document.getElementById('test_cov_hist'));"
                 "chart.draw(data, options);"
               "}"
+
+            "function drawFailResChart() {"
+                "var data = google.visualization.arrayToDataTable(["
+                  "['Number of failed test cases in a revision', 'Occurences'],"
+              "%3]);"
+
+              "var options = {"
+                  "title: 'Results fail histogram',"
+                  "legend: { position: 'none' },"
+                  "hAxis: {title: 'Number of failed test cases in a revision'},"
+                  "vAxis: {title: 'Number of revisions with the same number of failed test cases'}"
+              "};"
+              "var chart = new google.visualization.ColumnChart(document.getElementById('fail_res_hist'));"
+              "chart.draw(data, options);"
+            "}"
+            "function drawRevFailResChart() {"
+                "var data = google.visualization.arrayToDataTable(["
+                  "['Revision number', 'Number of failed test cases'],"
+              "%12]);"
+
+              "var options = {"
+                  "title: 'Revision fail histogram',"
+                  "legend: { position: 'none' },"
+                  "hAxis: {title: 'Revision number'},"
+                  "vAxis: {title: 'Number of failed test cases'}"
+              "};"
+              "var chart = new google.visualization.ColumnChart(document.getElementById('rev_fail_res_hist'));"
+              "chart.draw(data, options);"
+            "}"
+            "function drawTestCaseInfoResChart() {"
+                "var data = google.visualization.arrayToDataTable(["
+                  "['Test case id', 'Executed', 'Fail'],"
+              "%14]);"
+
+              "var options = {"
+                  "title: 'Test case info',"
+                  "legend: { position: 'none' },"
+                  "hAxis: {title: 'Test case id'},"
+                  "vAxis: {title: 'Number of Executed/Failed revisions'}"
+              "};"
+              "var chart = new google.visualization.ColumnChart(document.getElementById('tc_info_res'));"
+              "chart.draw(data, options);"
+            "}"
+            "function drawExecResChart() {"
+                "var data = google.visualization.arrayToDataTable(["
+                  "['Number of executed test cases', 'Occurences'],"
+              "%15]);"
+
+              "var options = {"
+                  "title: 'Executed histogram',"
+                  "legend: { position: 'none' },"
+                  "hAxis: {title: 'Number of executed revisions in a test case'},"
+                  "vAxis: {title: 'Number of test cases with the same number of execution'}"
+              "};"
+              "var chart = new google.visualization.ColumnChart(document.getElementById('exec_res_hist'));"
+              "chart.draw(data, options);"
+            "}"
             "</script></head><body><div>"
             "<h3>Coverage statistics</h3>"
-            "Number of code elements: %2</br>"
-            "Number of test cases: %3</br>"
-            "Density of the coverage matrix: %4</br>"
-            "Average test cases per code elements: %5</br>"
-            "Average code elements per test cases: %6</br>"
+            "Number of code elements: %4</br>"
+            "Number of test cases: %5</br>"
+            "Density of the coverage matrix: %6</br>"
+            "Average test cases per code elements: %7</br>"
+            "Average code elements per test cases: %8</br>"
             "<div id=\"code_cov_hist\" style=\"height:500px; width:1000px;\"></div>"
             "<div id=\"test_cov_hist\" style=\"height:500px; width:1000px;\"></div>"
             "</div>"
 
-            /*"<div><h3>Results statistics</h3>"
-            "Number of revisions: %8</br>"
-            "Number of test cases: %9</br>"
-            "Number of total failed test cases: %10</br>"
-            "<div id=\"exec_res_hist\"></div>"
-            "<div id=\"fail_res_hist\"></div>"
-            "<div id=\"rev_fail_res_hist\"></div>"
-            "<div id=\"tc_info_res\"></div></div>"*/
+            "<div><h3>Results statistics</h3>"
+            "Number of revisions: %9</br>"
+            "Number of test cases: %10</br>"
+            "Number of total failed test cases: %11</br>"
+            "Average failed test cases per revision: %13</br>"
+            "<div id=\"fail_res_hist\" style=\"height:500px; width:1000px;\"></div>"
+            "<div id=\"rev_fail_res_hist\" style=\"height:500px; width:1000px;\"></div>"
+            "<div id=\"exec_res_hist\" style=\"height:500px; width:1000px;\"></div>"
+            "<div id=\"tc_info_res\" style=\"height:500px; width:1000px;\"></div>"
+              "</div></body></html>";
 
-
-              "</body></html>";
-
-    rapidjson::Document* data = qobject_cast<CMainWindow*>(parent())->getWorkspace()->getResultsByName(COV_STATS);
+    rapidjson::Document *covData = qobject_cast<CMainWindow*>(parent())->getWorkspace()->getResultsByName(COV_STATS);
 
     QString codeHist;
-    convertJsonToStringArray(data, "code_coverage_histogram", codeHist);
+    convertJsonToStringArray(covData, "code_coverage_histogram", codeHist);
     QString testHist;
-    convertJsonToStringArray(data, "test_coverage_histogram", testHist);
-    QString execHist;
-    QString failHist;
-    QString revFailHist;
+    convertJsonToStringArray(covData, "test_coverage_histogram", testHist);
 
-    html = html.arg(codeHist, QString::number((*data)["number_of_code_elements"].GetInt()), QString::number((*data)["number_of_test_cases"].GetInt()), QString::number((*data)["density"].GetDouble()), QString::number((*data)["average_test_cases_per_code_elements"].GetDouble()), QString::number((*data)["average_code_elements_per_test_cases"].GetDouble()), testHist);
+    rapidjson::Document *resData = qobject_cast<CMainWindow*>(parent())->getWorkspace()->getResultsByName(RES_STATS);
+
+    QString execHist;
+    convertJsonToStringArray(resData, "executed_histogram", execHist);
+    QString failHist;
+    convertJsonToStringArray(resData, "fail_histogram", failHist);
+    QString revFailHist;
+    convertJsonToStringArray(resData, "revision_fail_histogram", revFailHist);
+    QString tcInfo;
+    const rapidjson::Value& val = (*resData)["test_case_info"];
+    for (rapidjson::Value::ConstMemberIterator itr = val.MemberBegin(); itr != val.MemberEnd(); ++itr) {
+        if (QString(itr->name.GetString()).compare("0") == 0)
+            continue;
+
+        tcInfo.append("['" + QString(itr->name.GetString()) + "'," + QString::number(itr->value["executed"].GetInt()) + "," +  QString::number(itr->value["fail"].GetInt()) + "],");
+    }
+    tcInfo.chop(1);
+
+    html = html.arg(codeHist,
+                    testHist,
+                    failHist,
+                    QString::number((*covData)["number_of_code_elements"].GetInt()),
+            QString::number((*covData)["number_of_test_cases"].GetInt()),
+            QString::number((*covData)["density"].GetDouble()),
+            QString::number((*covData)["average_test_cases_per_code_elements"].GetDouble()),
+            QString::number((*covData)["average_code_elements_per_test_cases"].GetDouble())).arg(
+            QString::number((*resData)["number_of_revisions"].GetInt()),
+            QString::number((*resData)["number_of_test_cases"].GetInt()),
+            QString::number((*resData)["number_of_total_fails"].GetInt()),
+            revFailHist,
+            QString::number((*resData)["average_failed_test_cases_per_revision"].GetDouble()),
+            tcInfo, execHist);
+
 
     ui->webView->settings()->clearMemoryCaches();
     ui->webView->setHtml(html);
@@ -102,10 +191,10 @@ void CShowStatisticsDialog::createHtml() {
 
 void CShowStatisticsDialog::convertJsonToStringArray(rapidjson::Document *data, QString element, QString &str)
 {
-    const rapidjson::Value& codeCov = (*data)[element.toStdString().c_str()];
-    for (rapidjson::Value::ConstMemberIterator itr = codeCov.MemberBegin(); itr != codeCov.MemberEnd(); ++itr) {
-        if (QString(itr->name.GetString()).compare("0") == 0)
-            continue;
+    const rapidjson::Value& val = (*data)[element.toStdString().c_str()];
+    for (rapidjson::Value::ConstMemberIterator itr = val.MemberBegin(); itr != val.MemberEnd(); ++itr) {
+        /*if (QString(itr->name.GetString()).compare("0") == 0)
+            continue;*/
 
         str.append("['" + QString(itr->name.GetString()) + "'," + QString::number(itr->value.GetInt()) + "],");
     }
