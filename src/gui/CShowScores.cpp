d2f36be9f25f4bb051f1ee689dc10282c3e8d8c9
David Havas <havasd@inf.u-szeged.hu>
2014-08-13 12:37:13 +0200

    Show fault localization score in one table.

--- src/gui/CShowScores.cpp
+++ src/gui/CShowScores.cpp
@@ -28,23 +28,32 @@ void CShowScores::generateResults(QWebView *view, rapidjson::Document *scores, C
                 "%2"
                 "</body></html>";
 
+    QString table = "data=new google.visualization.DataTable();"
+            "data.addColumn('string','Code Element');"
+            "%1"
+            "data.addRows(["
+              "%2"
+            "]);"
+            "table=new google.visualization.Table(document.getElementById('%3'));"
+            "table.draw(data,{page:'enable',pageSize:20,sortAscending:false,sortColumn:1});";
+
     QString callback;
     QString body;
     for (rapidjson::Value::ConstMemberIterator clusterIt = scores->MemberBegin(); clusterIt != scores->MemberEnd(); ++clusterIt) {
         QString cluster = clusterIt->name.GetString();
         body.append("<h4 style=\"text-align:center;\">" + cluster + "</h4>");
 
+        std::map<IndexType, std::map<String, double> > flScore;
         for (rapidjson::Value::ConstMemberIterator flIt = clusterIt->value.MemberBegin(); flIt != clusterIt->value.MemberEnd(); ++flIt) {
-            QString table = "data=new google.visualization.DataTable();"
-                    "data.addColumn('string','Code Element');"
-                    "data.addColumn('number','score');"
-                    "data.addRows(["
-                      "%1"
-                    "]);"
-                    "table=new google.visualization.Table(document.getElementById('%2'));"
-                    "table.draw(data,{page:'enable',pageSize:20,sortAscending:false,sortColumn:1});";
+            QString flName = flIt->name.GetString();
+            if (flName.contains("-fl")) {
+                for (rapidjson::Value::ConstMemberIterator scoreIt = flIt->value.MemberBegin(); scoreIt != flIt->value.MemberEnd(); ++scoreIt) {
+                    flScore[QString(scoreIt->name.GetString()).toLongLong()][flName.replace("-fl", "").toStdString()] = scoreIt->value.GetDouble();
+                }
+                continue;
+            }
 
-            body.append("<h5>" + QString(flIt->name.GetString()) + "</h5>");
+            body.append("<h5>" + flName + "</h5>");
             body.append("<div id=\"" + cluster + "_" + flIt->name.GetString() + "\"></div>");
             QString data;
             for (rapidjson::Value::ConstMemberIterator scoreIt = flIt->value.MemberBegin(); scoreIt != flIt->value.MemberEnd(); ++scoreIt) {
@@ -57,14 +66,40 @@ void CShowScores::generateResults(QWebView *view, rapidjson::Document *scores, C
                 else
                     codeElementName = selection->getCodeElements()->getValue(QString(scoreIt->name.GetString()).toLongLong()).c_str();
 
-
                 data.append("['" + codeElementName + "'," +
                             QString::number(scoreIt->value.GetDouble()) + "],");
             }
             data.chop(1);
-            table = table.arg(data, cluster + "_" + flIt->name.GetString());
-            callback.append(table);
+            callback.append(table.arg("data.addColumn('number','suspiciousness value');", data, cluster + "_" + flIt->name.GetString()));
         }
+
+        if (flScore.empty())
+            continue;
+
+        QString column;
+        QString data;
+        bool first = true;
+        for (std::map<IndexType, std::map<String, double> >::iterator ceIt = flScore.begin(); ceIt != flScore.end(); ++ceIt) {
+            QString codeElementName;
+            if (selection->getCodeElements()->size() == 0) // test suite not available
+                codeElementName = QString::number(ceIt->first);
+            else
+                codeElementName = selection->getCodeElements()->getValue(ceIt->first).c_str();
+
+            data.append("['" + codeElementName + "',");
+            for (std::map<String, double>::iterator scoreIt = ceIt->second.begin(); scoreIt != ceIt->second.end(); ++scoreIt) {
+                if (first)
+                    column.append("data.addColumn('number','" + QString(scoreIt->first.c_str()) + "');");
+                data.append(QString::number(scoreIt->second) + ",");
+            }
+            data.chop(1);
+            first = false;
+            data.append("],");
+        }
+        data.chop(1);
+        body.append("<h5>Fault localization score</h5>");
+        body.append("<div id=\"" + cluster + "_fl\"></div>");
+        callback.append(table.arg(column, data, cluster + "_fl"));
     }
 
     html = html.arg(callback, body);
