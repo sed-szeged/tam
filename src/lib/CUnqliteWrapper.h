2c2ea904032d669f55457ff2cd53ed6bf89be35d
David Havas <havasd@inf.u-szeged.hu>
2014-08-27 15:28:16 +0200

    Fixed fault-localization results saving.

--- src/lib/CUnqliteWrapper.h
+++ src/lib/CUnqliteWrapper.h
@@ -22,6 +22,8 @@ extern "C" {
  "}"\
  "/* Record to store the string is replaced with the proper json structure */ "\
  "$zRec = %s;"\
+ "if ($coll == 'score')"\
+ "  $zRec = json_encode($zRec);"\
  "$rc = db_store($coll, $zRec);"\
  "$recordId = db_last_record_id($coll);"
 
