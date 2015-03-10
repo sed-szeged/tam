6d5837d6fde3c622ed29108bb1fea8f70c46af90
David Havas <havasd@inf.u-szeged.hu>
2015-03-10 23:48:03 +0100

    Added copyright notice to source files.

--- src/gui/CShowClusters.cpp
+++ src/gui/CShowClusters.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C): 2014-2015 Department of Software Engineering, University of Szeged
+ *
+ * Authors: David Havas <havasd@inf.u-szeged.hu>
+ *
+ * This file is part of TAM.
+ *
+ *  TAM is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TAM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with TAM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "CShowClusters.h"
 
 CShowClusters::CShowClusters()
